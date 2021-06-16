0d7a1fa84b917014147b81762a524496759426a1
paigelynnv <64274185+paigelynnv@users.noreply.github.com>
2021-06-15 22:35:40 -0400

    add search form

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function displayTemp(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
   tempElement.innerHTML = Math.round(response.data.main.temp);
@@ -34,8 +35,20 @@ function displayTemp(response) {
   dateElement.innerHTML = formateDate(response.date.dt * 1000);
 }
 
-let apiKey = "2fdf8a69bf0d67738ab9917d87bd1637";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}
-&units=metric`;
+function search {
+  let apiKey = "2fdf8a69bf0d67738ab9917d87bd1637";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
+  &units=metric`;
+  axios.get(apiUrl).then(displayTemp);
+}
 
-axios.get(apiUrl).then(displayTemp);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElemenet = document.querySelector("#city-input");
+  search(cityInputElemenet.value);
+}
+
+search("");
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
